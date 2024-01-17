fcc61455d6384cb5b71c8536030da22df42809e0
Ryan <rytmcnally@gmail.com>
2024-01-17 10:22:58 -0800

    Removed commented out code

--- sql_database.py
+++ sql_database.py
@@ -25,15 +25,7 @@ class Database:
     def drop_all_tables(self):
         self.execute_list_commands(self.drop_table_commands)
 
-    # TODO - clean up which of these are actually used
-    # def execute(self, command):
-    #     """Execute a single command"""
-    #     con = sqlite3.connect(self.database)
-    #     cur = con.cursor()
-    #     cur.execute(command)
-    #     con.commit()
-    #     con.close()
-
+    # Execute
     def execute(self, command, params=None):
         """Execute a single command"""
         con = sqlite3.connect(self.database)
@@ -55,48 +47,7 @@ class Database:
         for command in command_list:
             self.execute(command)
 
-    # def execute_parameters(self, command, parameters):
-    #     """Execute a single command with parameters"""
-    #     con = sqlite3.connect(self.database)
-    #     cur = con.cursor()
-    #     cur.execute(command, parameters)
-    #     con.commit()
-    #     con.close()
-    #
-    # def execute_kwargs(self, command, kwargs):
-    #     con = sqlite3.connect(self.database)
-    #     cur = con.cursor()
-    #     cur.execute(command, kwargs)
-    #     con.commit()
-    #     con.close()
-
-    # def sql_fetch_all_dict(self, command_text):
-    #     con = sqlite3.connect(self.database)
-    #     con.row_factory = dict_factory
-    #     cur = con.cursor()
-    #     result = cur.execute(command_text).fetchall()
-    #     con.commit()
-    #     con.close()
-    #     return result
-    #
-    # def sql_fetch_all_dict_params(self, command_text, params):
-    #     con = sqlite3.connect(self.database)
-    #     con.row_factory = dict_factory
-    #     cur = con.cursor()
-    #     result = cur.execute(command_text, params).fetchall()
-    #     con.commit()
-    #     con.close()
-    #     return result
-
-    # def sql_fetch_one_params(self, command_text, params):
-    #     con = sqlite3.connect(self.database)
-    #     con.row_factory = dict_factory
-    #     cur = con.cursor()
-    #     result = cur.execute(command_text, params).fetchone()
-    #     con.commit()
-    #     con.close()
-    #     return result
-
+    # Fetch
     def sql_fetch_one(self, command, params=None):
         con = sqlite3.connect(self.database)
         con.row_factory = dict_factory
