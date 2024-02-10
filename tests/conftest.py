d6b3340ab9347827bff080f86d4a183b360bb5ce
Ryan <rytmcnally@gmail.com>
2024-02-09 20:33:20 -0800

    Delete test_lookup.py
    Move insert dict into conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,19 @@
 import finance_tool
 import pytest
-import tests.test_lookup as test_lookup
 import json
+
+insert_dict = {'account': finance_tool.insert_account,
+               'account_type': finance_tool.insert_account_type,
+               'allocation': finance_tool.insert_allocation,
+               'asset': finance_tool.insert_asset,
+               'asset_class': finance_tool.insert_asset_class,
+               'balance': finance_tool.insert_balance,
+               'component': finance_tool.insert_component,
+               'constant': finance_tool.insert_constant,
+               'institution': finance_tool.insert_institution,
+               'location': finance_tool.insert_location,
+               'owner': finance_tool.insert_owner,
+               'price': finance_tool.insert_price}
 
 
 def json_loader(file_name):
@@ -23,7 +35,7 @@ def test_db_1(tmp_path):
     finance_tool.execute_file(db_test, '../db.sql')
     data = json_loader('../tests/data/test_db_1.json')
     for table_name in data:
-        finance_tool.execute_many(database=db_test, cmd=test_lookup.insert_dict[table_name], data_sequence=data[table_name])
+        finance_tool.execute_many(database=db_test, cmd=insert_dict[table_name], data_sequence=data[table_name])
 
     return db_test
 
@@ -35,6 +47,6 @@ def test_db_2(tmp_path):
     finance_tool.execute_file(db_test, '../db.sql')
     data = json_loader('../tests/data/test_db_2.json')
     for table_name in data:
-        finance_tool.execute_many(database=db_test, cmd=test_lookup.insert_dict[table_name], data_sequence=data[table_name])
+        finance_tool.execute_many(database=db_test, cmd=insert_dict[table_name], data_sequence=data[table_name])
 
     return db_test
